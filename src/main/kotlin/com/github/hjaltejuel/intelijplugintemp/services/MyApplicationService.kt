02878565b1e307b03c84f2a61685cdadd672c17a
GitHub Action <action@github.com>
2021-04-19 20:44:21 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/hjaltejuel/intelijplugintemp/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.hjaltejuel.intelijplugintemp.services
+
+import com.github.hjaltejuel.intelijplugintemp.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/hjaltejuel/intelijplugintemp/services/MyProjectService.kt
+++ src/main/kotlin/com/github/hjaltejuel/intelijplugintemp/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.hjaltejuel.intelijplugintemp.services
 
+import com.github.hjaltejuel.intelijplugintemp.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
